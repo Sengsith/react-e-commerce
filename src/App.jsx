0698e79488e6cce2fc4eb7e33e04487d5780b141
Angelo Rosario <angelorosario10@gmail.com>
2023-08-14 13:09:08 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+// TODO: Lookup later: https://www.w3.org/WAI/ARIA/apg/
+
+export default App;
