d78ff1a0d23a3bb72e50fddb012e24afa70b1d15
Angelo Rosario <angelorosario10@gmail.com>
2023-08-16 16:00:20 -0700

    Header styling finished, menu not finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Header.css";
 
 const Header = () => {
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+
+  const toggleMenu = () => {
+    // Invert isMenuExpanded
+    setIsMenuExpanded((prevIsMenuExpanded) => !prevIsMenuExpanded);
+  };
+
   return (
     <header className="primary-header">
-      <nav>
-        <button
-          id="hamburger"
-          aria-haspopup="true"
-          aria-expanded="false"
-          aria-label="Menu"
-          aria-controls="nav-menu"
-        >
-          <img
-            id="hamburger-icon"
-            src="./src/assets/images/icon-menu.svg"
-            alt="hamburger-menu-icon"
-          ></img>
-        </button>
-        <ul id="nav-menu" aria-labelledby="hamburger">
-          <a href="#">Collections</a>
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </ul>
-      </nav>
-      <img src="./src/assets/images/logo.svg" alt="sneakers"></img>
-      <img src="./src/assets/images/icon-cart.svg" alt="cart"></img>
-      <img src="./src/assets/images/image-avatar.png"></img>
+      <div className="header-left">
+        <nav>
+          <button
+            id="hamburger-btn"
+            onClick={toggleMenu}
+            aria-haspopup="true"
+            aria-expanded="false"
+            aria-label="Menu"
+            aria-controls="nav-menu"
+          >
+            <img
+              id="icon-hamburger"
+              src="./src/assets/images/icon-menu.svg"
+              alt="hamburger-menu-icon"
+            />
+          </button>
+          {isMenuExpanded && (
+            <ul id="nav-menu" aria-labelledby="hamburger">
+              <a href="#">Collections</a>
+              <a href="#">Men</a>
+              <a href="#">Women</a>
+              <a href="#">About</a>
+              <a href="#">Contact</a>
+            </ul>
+          )}
+        </nav>
+        <img src="./src/assets/images/logo.svg" alt="sneakers-logo"></img>
+      </div>
+      <div className="header-right">
+        <img
+          id="icon-cart"
+          src="./src/assets/images/icon-cart.svg"
+          alt="cart"
+        ></img>
+        <img
+          id="avatar"
+          src="./src/assets/images/image-avatar.png"
+          alt="avatar-headshot"
+        ></img>
+      </div>
     </header>
   );
 };
