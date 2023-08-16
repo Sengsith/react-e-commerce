28738d9586a9a90511aaad1faee29c571ac2a027
Angelo Rosario <angelorosario10@gmail.com>
2023-08-16 12:37:00 -0700

    Updated key to unique id instead of array index

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,18 +3,22 @@ import React from "react";
 const Carousel = () => {
   const gallery = [
     {
+      id: 1,
       thumbnail: "../../src/assets/images/image-product-1-thumbnail.jpg",
       image: "../../src/assets/images/image-product-1.jpg",
     },
     {
+      id: 2,
       thumbnail: "../../src/assets/images/image-product-2-thumbnail.jpg",
       image: "../../src/assets/images/image-product-2.jpg",
     },
     {
+      id: 3,
       thumbnail: "../../src/assets/images/image-product-3-thumbnail.jpg",
       image: "../../src/assets/images/image-product-3.jpg",
     },
     {
+      id: 4,
       thumbnail: "../../src/assets/images/image-product-4-thumbnail.jpg",
       image: "../../src/assets/images/image-product-4.jpg",
     },
@@ -22,8 +26,8 @@ const Carousel = () => {
 
   return (
     <div className="carousel">
-      {gallery.map((image, index) => {
-        return <img key={index} src={image.thumbnail}></img>;
+      {gallery.map((image) => {
+        return <img key={image.id} src={image.thumbnail}></img>;
       })}
     </div>
   );
