675f9561313b92fa4ccddc0023b6f10b2406f6d8
Angelo Rosario <angelorosario10@gmail.com>
2023-08-21 17:49:17 -0700

    mobile carousel functionality complete

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./carousel.css";
 
@@ -26,19 +26,62 @@ const Carousel = () => {
     },
   ];
 
+  const [leftId, setLeftId] = useState(1);
+  const [rightId, setRightId] = useState(1);
+
+  const decrementArrowImageId = () => {
+    // First element
+    if (leftId === gallery[0].id) {
+      setLeftId(gallery.length);
+    } else {
+      setLeftId((prevLeftId) => prevLeftId - 1);
+    }
+  };
+
+  const incrementArrowImageId = () => {
+    // Last element
+    if (rightId === gallery.length) {
+      setRightId(gallery[0].id);
+    } else {
+      setRightId((prevLeftId) => prevLeftId + 1);
+    }
+  };
+
+  const handleClickPrevious = () => {
+    decrementArrowImageId();
+  };
+
+  const handleClickNext = () => {
+    incrementArrowImageId();
+  };
+
   return (
-    <div className="carousel">
-      <div className="arrow-buttons">
-        <button className="arrow" id="left-arrow">
-          <img src="../../src/assets/images/icon-previous.svg" />
-        </button>
-        <button className="arrow" id="right-arrow">
-          <img src="../../src/assets/images/icon-next.svg" />
-        </button>
-      </div>
-      <div className="carousel-images">
+    <div className="carousel-container">
+      <a
+        href={`#image-${leftId}`}
+        className="carousel-arrow"
+        id="previous-arrow"
+        aria-label="previous-image"
+        onClick={handleClickPrevious}
+      >
+        <img src="../../src/assets/images/icon-previous.svg" alt="left-arrow" />
+      </a>
+      <a
+        href={`#image-${rightId}`}
+        className="carousel-arrow"
+        id="next-arrow"
+        aria-label="next-image"
+        onClick={handleClickNext}
+      >
+        <img src="../../src/assets/images/icon-next.svg" alt="right-arrow" />
+      </a>
+      <div className="carousel-wrapper">
         {gallery.map((image) => {
-          return <img key={image.id} src={image.thumbnail}></img>;
+          return (
+            <div key={image.id} className="carousel-element">
+              <img id={`image-${image.id}`} src={image.thumbnail}></img>
+            </div>
+          );
         })}
       </div>
     </div>
