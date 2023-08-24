8b1d230f01a7e4bbc0b2cd666c8d2c656429a99b
Angelo Rosario <angelorosario10@gmail.com>
2023-08-24 15:27:42 -0700

    Implemented nav menu in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "./header.css";
 
 import HamburgerIcon from "/src/assets/images/icon-menu.svg";
+import CloseIcon from "/src/assets/images/icon-close.svg";
 import SneakersLogo from "/src/assets/images/logo.svg";
 import Avatar from "/src/assets/images/image-avatar.png";
 
@@ -12,14 +13,22 @@ const Header = () => {
   const [isMenuExpanded, setIsMenuExpanded] = useState(false);
 
   const toggleMenu = () => {
+    const navMenu = document.querySelector("#header-nav");
+    const menuBg = document.querySelector("#menu-bg");
+
     // Toggle isMenuExpanded
     setIsMenuExpanded((prevIsMenuExpanded) => !prevIsMenuExpanded);
+
+    // Also want to toggle menu-bg-active and header-nav-active
+    navMenu.classList.toggle("header-nav-active");
+    menuBg.classList.toggle("menu-bg-active");
   };
 
   return (
     <header className="primary-header">
       <div className="header-left">
-        <nav>
+        <div id="menu-bg"></div>
+        <nav id="header-nav">
           <button
             id="hamburger-btn"
             onClick={toggleMenu}
@@ -30,17 +39,27 @@ const Header = () => {
           >
             <img
               id="icon-hamburger"
-              src={HamburgerIcon}
+              src={isMenuExpanded ? CloseIcon : HamburgerIcon}
               alt="hamburger-menu-icon"
             />
           </button>
           {isMenuExpanded && (
             <ul id="nav-menu" aria-labelledby="hamburger">
-              <a href="#">Collections</a>
-              <a href="#">Men</a>
-              <a href="#">Women</a>
-              <a href="#">About</a>
-              <a href="#">Contact</a>
+              <a className="nav-menu-item" href="#">
+                Collections
+              </a>
+              <a className="nav-menu-item" href="#">
+                Men
+              </a>
+              <a className="nav-menu-item" href="#">
+                Women
+              </a>
+              <a className="nav-menu-item" href="#">
+                About
+              </a>
+              <a className="nav-menu-item" href="#">
+                Contact
+              </a>
             </ul>
           )}
         </nav>
